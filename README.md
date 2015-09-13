8b57ad3ac68e22d2df463df5b689e94ccffb2d01
Joe Schmeling <jinsd6@gmail.com>
2015-09-13 11:24:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+####Couture by the Sea
+This site will track invenory and expense to simplifiy our life.
---
+++ inventory.php
@@ -0,0 +1,30 @@
+<?php // sqltest.php
+    require_once 'login.php';
+    
+    try
+        {
+            $db = new PDO($dsn, $un, $pw);
+        }
+    catch(PDOException $e)
+        {die($e->getMessage());}
+    
+    $query = "SELECT * FROM inventory";
+    $result = $db->query($query);
+    if (!$result) die ("Database access failed: " . $conn->error);
+    
+    //$rows = $result->num_rows;
+    
+    echo"<table cellpadding=10 border=1>";
+    $rowarray = $result->fetchAll(PDO::FETCH_OBJ);
+    foreach($rowarray as $row)
+    {
+        echo "<tr>";
+        echo "<td>".$row->Item."</td>";
+        echo "<td>".$row->Description."</td>";
+        echo "<td>".$row->Price."</td>";
+    }
+    
+    echo "</table>";
+    
+    $db = null;
+?>

dd516e351a70453f9e9b31810a6b900ca43d7f50
Joe Schmeling <jinsd6@gmail.com>
2016-05-16 19:43:57 -0700

    Added function for database connection

--- inventory.php
+++ inventory.php
@@ -1,22 +1,34 @@
 <?php // sqltest.php
-    require_once 'login.php';
     
-    try
-        {
-            $db = new PDO("pgsql:host=$hn port=5432 dbname=$db user=$un password=$pw");
+    function db_connect()
+    {
+        require_once 'login.php';
+
+        static $connection;
+
+        if(!isset($connection)) {
+            try {
+                $connection = new PDO("pgsql:host=$hn port=5432 dbname=$db user=$un password=$pw");
+                return $connection;
+            }
+            catch(PDOException $e)
+            {
+                die($e->getMessage());
+            }
         }
-    catch(PDOException $e)
-        {die($e->getMessage());}
-    
-    $query = "SELECT * FROM inventory";
+    }
+
+    $db = db_connect();
+    $query = "select inv.item, inv.description, inv.price, loc.name from inventory inv INNER JOIN location loc ON inv.location = loc.id";
     $result = $db->query($query);
     if (!$result) die ("Database access failed: " . $conn->error);
 
     echo "<title>Couture by the Sea</title><link rel='stylesheet' " .
          "href='styles.css' type='text/css'>" .
-         "<div class='main'>Couture by the Sea" .
+         "<div class='main'>".
+            "<h1>Couture by the Sea" .
          "<br>" .
-         "Inventory Tracker</div>";
+         "Inventory Tracker</h1></div>";
 
     echo "<br>";
     echo "<br>";
@@ -28,6 +40,7 @@
         echo "<th>Item</th>";
         echo "<th>Description</th>";
         echo "<th>Price</th>";
+        echo "<th>Location</th>";
 
         $rowarray = $result->fetchAll(PDO::FETCH_OBJ);
         foreach($rowarray as $row)
@@ -36,6 +49,7 @@
             echo "<td>".$row->item."</td>";
             echo "<td>".$row->description."</td>";
             echo "<td>".$row->price."</td>";
+            echo "<td>".$row->name."</td>";
         }
 
         echo "</table>";
